33604ec2d53bdcc0a79ca95eb8fba74154e0101c
DigitalConfections <66529806+OpenARDF@users.noreply.github.com>
2024-06-07 13:41:39 -0400

    Ver 0.13
    Sets PWM frequency to 2.5 kHz

--- Software/AVR128DA48/SignalSnagger/src/tca.cpp
+++ Software/AVR128DA48/SignalSnagger/src/tca.cpp
@@ -65,7 +65,8 @@ void TIMERA_init(void)
 #else
 //	TCA0.SINGLE.PER = 0x0950; /* Set timer period to 10 kHz */
 //	TCA0.SINGLE.PER = 0x03E8; /* Set timer period to 24 kHz */
-	TCA0.SINGLE.PER = 0x12A0; /* Set timer period to 5 kHz */
+//	TCA0.SINGLE.PER = 0x12A0; /* Set timer period to 5 kHz */
+	TCA0.SINGLE.PER = 0x2580; /* Set timer period to 2.5 kHz */
 	PORTMUX.TCAROUTEA = PORTMUX_TCA0_PORTE_gc; /* PWM WO2 out on PE2 */
 	TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP2EN_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc; /* Set waveform generation mode single-slope, and enable compare channel */
 	setPWM(MAX_PWM_SETTING/2); /* Start at 50% duty cycle */
